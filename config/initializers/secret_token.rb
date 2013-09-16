4917c73a11e3a6c1175f60e857305ca2119fff4a
Mohammed Ameen <ameen@keepworks.com>
2013-09-16 16:13:04 +0530

    Added Controllers for Welcome, Posts and a view for index and changed the routes.rb to point to the index.html.erb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '95ef1cc6070620bcd376a4a85950f9b023d1ba835c1d187cd2404c67e1d6ab38509ab12cae62474e0334afaa6def76a50738c8694d2e436408ab425ba21c1bef'
